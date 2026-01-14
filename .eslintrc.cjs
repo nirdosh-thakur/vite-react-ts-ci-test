b126074213d897e26a678d117d5285e907be5a3c
nirdosh-thakur <nirdosh.1.thakur@gmail.com>
2026-01-14 22:53:07 +0530

    Added ci.yml for continuous integrations

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+}
